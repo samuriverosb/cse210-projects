d392b6b59cc6aa00dd47cf1065a151a19f997b48
samuriverosb <samuel.riverosb@gmail.com>
2023-01-05 16:02:03 -0300

    C# Prep3 Completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int randomNumber = random.Next(1, 100);
+        int guessNumber;
+        int guesses = 0;
+
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            guessNumber = int.Parse(guess);
+            if (guessNumber < randomNumber)
+            {
+                Console.WriteLine("Higher");
+                guesses += 1;
+            }
+            else if (guessNumber > randomNumber)
+            {
+                Console.WriteLine("Lower");
+                guesses += 1;
+            }
+            else
+            {
+                guesses += 1;
+                Console.WriteLine($"You guessed it! It took you {guesses} intents");
+            }
+        } while (guessNumber != randomNumber);
+
     }
 }
