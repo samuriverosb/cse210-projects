146ff4f3b09051351358e4502c95735cd94dede1
samuriverosb <samuel.riverosb@gmail.com>
2023-01-06 13:08:00 -0300

    C# Prep5 Completed

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        DisplayResult(name, number);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string numberStr = Console.ReadLine();
+        int number = int.Parse(numberStr);
+        return number;
+    }
+
+    static void DisplayResult(string name, int number)
+    {
+        number = number * number;
+        Console.WriteLine($"{name}, the square of your number is {number}");
     }
 }
