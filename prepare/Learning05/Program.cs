c566dd664b1cce1cccd109498d9e07cb6f2ba515
samuriverosb <samuel.riverosb@gmail.com>
2023-03-06 16:05:41 -0300

    Lesson05 completed

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Red", 2.5);
+
+        Rectangle rectangle = new Rectangle("Blue", 2.5, 3);
+
+        Circle circle = new Circle("Yellow", 1);
+
+        List<Shape> listOfShapes = new List<Shape>();
+        listOfShapes.Add(square);
+        listOfShapes.Add(rectangle);
+        listOfShapes.Add(circle);
+
+        foreach(Shape shape in listOfShapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+            Console.WriteLine();
+        }
     }
 }
